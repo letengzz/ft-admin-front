32b2d0c5f44d0f6c190f7bad20d13b478fce60bf
letengzz <2020885569@qq.com>
2025-09-04 22:24:09 +0800

    feat: add styles&mixin&variables

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,16 @@
 
 <template>
   <main>
+    <div>
+      <h1 class="num">This is an about page</h1>
+    </div>
     <HelloWorld msg="Hello World" />
   </main>
 </template>
+<style lang="scss" scoped>
+.num {
+  @include wh(500, 500);
+
+  color: $red;
+}
+</style>
